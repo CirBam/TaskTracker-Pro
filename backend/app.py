860fd8acd901fa820d11c8578952f7747a92ace8
mabric <mabriccorcuera@gmail.com>
2026-06-23 10:01:48 -0400

    Fixed comments

--- backend/app.py
+++ backend/app.py
@@ -31,9 +31,9 @@ def create_app():
         For registering a new user
         """
         data = request.json
-        status, user = auth.register(data.get('username'), data.get('password')) # For authenticating the user
+        status, user = auth.register(data.get('username'), data.get('password')) # For better safe handling of data
 
-        if status == auth.REGISTER_SUCCESS: # Actually testing if it authenticates
+        if status == auth.REGISTER_SUCCESS: # Actually testing if it registers
             return jsonify({"message": "User registered successfully"}), 201 # 201 is for the HTML status code, 201 = Created
         return jsonify({"error": status}), 400 # For basic troubleshooting, 400 = Bad Request
 
